e92de03ceb7e8b1b47ec1d0b7be95f8da1ae0645
Deniz Heinzelmann <deniz.heinzelmann@gmx.de>
2024-04-25 14:43:43 +0200

    Older Quiz-App

---
+++ js/questions.js
@@ -0,0 +1,90 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of these countries did the Soviet Union NEVER invade?",
+    answer: "Sweden",
+    options: [
+      "Afghanistan",
+      "Finland",
+      "Poland",
+      "Sweden"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who was the first person to orbit the Earth?",
+    answer: "Yuri Gagarin",
+    options: [
+      "Neil Armstrong",
+      "Yuri Gagarin",
+      "John Glenn",
+      "Valentina Tereshkova"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In which of Shakespeares plays does the Ghost of a King tell his son to avenge his murder?",
+    answer: "Hamlet",
+    options: [
+      "Julius Caesar",
+      "Macbeth",
+      "Hamlet",
+      "Romeo and Juliet"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Whats the name of the Polish Pianist and Holocaust Survivor 'The Pianist'?",
+    answer: "Władysław Szpilman",
+    options: [
+      "Frédéric Chopin",
+      "Aleksander Michałowski",
+      "Władysław Szpilman",
+      "Hania Rani"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which famous phrase is attributed to Julius Caesar upon crossing the Rubicon River?",
+    answer: "Alea iacta es",
+    options: [
+      "Alea iacta es",
+      "Veni, vidi, vici",
+      "Senatus Populusque Romanus",
+      "Et tu, Brute?"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who was the first President of the United States?",
+    answer: "George Washington",
+    options: [
+      "Thomas Jefferson",
+      "George Washington",
+      "John Adams",
+      "Benjamin Franklin"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The French Revolution began with the storming of which famous Parisian fortress on July 14, 1789?",
+    answer: "Bastille",
+    options: [
+      "Louvre",
+      "Notre Dame Cathedral",
+      "Versailles",
+      "Bastille"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Who was the leader of the Soviet Union and Italy during World War II?",
+    answer: "Joseph Stalin and Benito Mussolini",
+    options: [
+      "Joseph Stalin and Giovanni Giolitti",
+      "Vladimir Lenin and Giuseppe Garibaldi",
+      "Joseph Stalin and Benito Mussolini",
+      "Nikita Chruschtschow and Victor Emmanuel III."
+    ]
+  },
+];
